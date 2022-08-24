5039a709e0e8730e0325379f5c45b28bfcfa1aeb
Pedro Henrique Cursino Cabral <97694134+WorenoKing@users.noreply.github.com>
2022-08-24 14:06:10 -0300

    Excluir Atualizado

--- Feed/admin/excluir-artigo.php
+++ Feed/admin/excluir-artigo.php
@@ -18,23 +18,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <head>
     <link rel="stylesheet" type="text/css" href="../style.css">
+    <!-- font icons from bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./adminStyle.css">
     <meta charset="UTF-8">
     <title>Excluir Artigo</title>
 </head>
 
 <body>
-    <div id="container">
-        <h1>Você realmente deseja excluir o artigo?</h1>
-        <form method="post" action="excluir-artigo.php">
-            <p style="text-align:center;">
-                <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>" />
-                <button class="botao">Excluir</button>
-            </p>
-        </form>
+<div id="container">
+        <h1 class="excluir_h1"><i class="fa fa-exclamation-triangle excluir_icon" aria-hidden="true"></i> Você realmente deseja excluir o artigo? <i class="fa fa-exclamation-triangle excluir_icon" aria-hidden="true"></i></h1>
+        <section class="btns">
+            <form method="post" action="excluir-artigo.php">
+                <p class="botao-area">
+                    <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>" />
+                    <button class="botao">Excluir <i class="fa fa-trash" aria-hidden="true"></i></button>
+                </p>
+            </form>
+            <div class="btn-back">
+                <button class="botao-back" data-back><i class="fa fa-arrow-left" aria-hidden="true"></i> Voltar</button>
+            </div>
+        </section>
     </div>
     <?php
     include('../../assets/footer.php');
     ?>
+    <script src="../../javascript/admin-btns.js"></script>
 </body>
 
 </html>
